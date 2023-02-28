f745f5a0801579da48665c1df6864d799768c2a1
enrique <okay.enrique@gmail.com>
2023-02-28 01:03:10 +0000

    lab done

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Lab {
@@ -30,6 +31,12 @@ public class Lab {
      * @return a string representation of all values of nums, with the values separated by spaces.
      */
     public String arrayToString(int[] nums){
-        return "";
+        String str = new String();
+
+        for (int i = 0; i < nums.length; i++) {
+        str += nums[i] + " ";
+    }
+
+        return str;
     }
 }
